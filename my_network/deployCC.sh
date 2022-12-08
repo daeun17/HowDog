f7fbe91c3b0a327f827a34601aa82dafc4ecd11f
daeun17 <daeun71@gmail.com>
2022-12-08 17:08:38 +0000

    2022.12.09

--- my_network/deployCC.sh
+++ my_network/deployCC.sh
@@ -23,11 +23,11 @@ export FABRIC_CFG_PATH=${PWD}/config
 
 # CHANNEL_NAME="mychannel"
 # 변수 지정 및설정
-CC_NAME="mycar"
-CC_SRC_PATH="/home/bstudent/fabric-samples/chaincode/fabcar/go"
+CC_NAME="howdog"
+CC_SRC_PATH="/home/bstudent/dev/my_project/contract/howdog/v0.9"
 CC_RUNTIME_LANGUAGE="golang"
-CC_VERSION="1"
-CHANNEL_NAME="mychannel"
+CC_VERSION="0.9"
+CHANNEL_NAME="howmuchnet"
 
 
 ## 1 package the chaincode
@@ -60,6 +60,26 @@ export CORE_PEER_LOCALMSPID="Org2MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
 export CORE_PEER_ADDRESS=localhost:9051
+
+set -x
+peer lifecycle chaincode install ${CC_NAME}.tar.gz >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+
+set -x
+peer lifecycle chaincode queryinstalled >&log.txt  
+{ set +x; } 2>/dev/null
+PACKAGE_ID=$(sed -n "/${CC_NAME}_${CC_VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
+
+
+#2.3 Install chaincode on peer0.org3
+infoln "Installing chaincode on peer0.org3..."
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org3MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+export CORE_PEER_ADDRESS=localhost:11051
 
 set -x
 peer lifecycle chaincode install ${CC_NAME}.tar.gz >&log.txt
@@ -109,6 +129,23 @@ peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameO
 { set +x; } 2>/dev/null
 cat log.txt
 
+## 3.3 approve the definition for org3
+infoln "approve the definition on peer0.org3..."
+
+ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org3MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+export CORE_PEER_ADDRESS=localhost:11051
+
+
+set -x
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${CC_VERSION} --package-id ${PACKAGE_ID} --sequence 1 >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+
 ## check commitreadiness
 peer lifecycle chaincode checkcommitreadiness --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${CC_VERSION} --sequence 1 --tls --cafile $ORDERER_CA --output json
 
@@ -126,18 +163,27 @@ cat log.txt
 peer lifecycle chaincode querycommitted --channelID $CHANNEL_NAME --name ${CC_NAME} --cafile $ORDERER_CA
 
 
-## TEST1 : Invoking the chaincode
+## TEST1 : Invoking the chaincode -> Receipt_register, Progno_receipt, Verify_receipt
 infoln "TEST1 : Invoking the chaincode"
 set -x
-peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n ${CC_NAME} $PEER_CONN_PARMS -c '{"function":"InitLedger","Args":[]}' >&log.txt
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n ${CC_NAME} $PEER_CONN_PARMS -c '{"function":"Receipt_register","Args":["R10001","bstudent","busanhospital,20221206","100000"]}' >&log.txt
 { set +x; } 2>/dev/null
 cat log.txt
 sleep 3
 
-## TEST2 : Query the chaincode
+## TEST2 : Query the chaincode -> Query_record, GetHistory
 
 infoln "TEST2 : Query the chaincode"
 set -x
-peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"Args":["QueryAllCars"]}' >&log.txt
+peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"Args":["Query_record", "R10001"]}' >&log.txt
 { set +x; } 2>/dev/null
 cat log.txt
+
+##Test3 : history chaincode 
+
+peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"Args":["GetHistory", "R10001"]}'
+
+##Test4 : invoke Progno_receipt chaincode -> tx이 만들어져야 하니까 invoke
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n ${CC_NAME} $PEER_CONN_PARMS -c '{"function":"Progno_receipt","Args":["R10001","good, qured"]}'
+
